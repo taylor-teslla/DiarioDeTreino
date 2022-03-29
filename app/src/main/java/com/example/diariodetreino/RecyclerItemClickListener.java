84e5631a8078ec3b06365634a848518e2811ef64
taylor-teslla <taylorteslla@gmail.com>
2022-03-29 14:10:44 -0300

    xxx

---
+++ app/src/main/java/com/example/diariodetreino/RecyclerItemClickListener.java
@@ -0,0 +1,10 @@
+package com.example.diariodetreino;
+
+import android.content.Context;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerItemClickListener implements RecyclerView.OnItemTouchListener {
+    public RecyclerItemClickListener(Context applicationContext, RecyclerItemClickListener.OnItemClickListener onItemClickListener) {
+    }
+}
